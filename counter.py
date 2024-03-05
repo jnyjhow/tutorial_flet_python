9b9121df70daae83b037bd5d772db8a1499cbc5e
jnyjhow <jhonatan_yamane@hotmail.com>
2024-03-05 19:24:01 -0300

    adaptando modo web.

--- counter.py
+++ counter.py
@@ -25,5 +25,5 @@ def main(page: ft.Page):
         )
     )
 
-ft.app(target=main)
-# ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+# ft.app(target=main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
